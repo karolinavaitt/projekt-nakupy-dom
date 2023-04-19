21e6fade20cc90788589e5fe5a61f3481fddc5a8
Martin Podloucký <podlouckymartin@gmail.com>
2023-04-19 11:03:53 +0200

    Odstranění ternárního operátoru

--- ShopList/index.js
+++ ShopList/index.js
@@ -3,18 +3,23 @@ import { ListItem } from '../ListItem/index.js';
 export const ShopList = (props) => {
   const { day, dayResult } = props;
 
+  let dayName = 'Načítám...';
+  if (dayResult !== 'loading') {
+    dayName = dayResult.dayName;
+  }
+
   const element = document.createElement('div');
   element.classList.add('shoplist');
   element.innerHTML = `
     <div class="shoplist__head">
-      <h2 class="shoplist__day">
-        ${dayResult === 'loading' ? 'Načítám...' : dayResult.dayName}
-      </h2>
+      <h2 class="shoplist__day">${dayName}</h2>
     </div>
     <div class="shoplist__items"></div>
   `;
 
   if (dayResult === 'loading') {
+    
+    
     fetch(`https://nakupy.kodim.app/api/sampleweek/${day}`)
       .then((response) => response.json())
       .then((data) => {
